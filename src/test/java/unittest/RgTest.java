009b136bc69473bd34d51fa14853163f0c9655df
Rubens Lobo <rubens.lobo1287@yahoo.com.br>
2020-07-06 09:21:03 -0300

    add testes

--- src/test/java/unittest/RgTest.java
+++ src/test/java/unittest/RgTest.java
@@ -9,12 +9,11 @@ public class RgTest {
 	
 	RG rg = new RG();
 	
-	/*@Test
+	@Test
 	public void testIsValidoRG() {
 		Assert.assertFalse("Valida RG Nulo: ",rg.isValidaRG(null));
 		Assert.assertFalse("Valida RG Nulo: ",rg.isValidaRG(""));
 		Assert.assertFalse("Valida RG sem traço: ",rg.isValidaRG("335073138"));
 		Assert.assertTrue("Valida RG com um traço: ", rg.isValidaRG("33507313-X"));
 	}
-*/
 }
