7bd447b96bc42d4a96501e4dede7b86c01c72634
Rubens Lobo <rubens.lobo1287@yahoo.com.br>
2020-07-05 22:10:02 -0300

    add sonar

--- src/main/java/tdd/RG.java
+++ src/main/java/tdd/RG.java
@@ -1,9 +1,10 @@
 package tdd;
 
+
 public class RG {
 	
 	public boolean isValidaRG(String rg) {
-		if((rg == null) || (rg.isEmpty()) || (rg.charAt(8) != '-')) {
+		if((rg == null) ||  (rg.isEmpty()) || (rg.charAt(8) != '-')) {
 			return false;
 		}
 		return true;
--- src/test/java/unittest/RgTest.java
+++ src/test/java/unittest/RgTest.java
@@ -9,11 +9,12 @@ public class RgTest {
 	
 	RG rg = new RG();
 	
-	@Test
+	/*@Test
 	public void testIsValidoRG() {
-		Assert.assertTrue("Valida RG Nulo: ",rg.isValidaRG("33507313-8"));
-		Assert.assertTrue("Valida RG Vazio: ",rg.isValidaRG("33507313-8"));
-		Assert.assertTrue("Valida RG Tem um traço: ", rg.isValidaRG("33507313-X"));
+		Assert.assertFalse("Valida RG Nulo: ",rg.isValidaRG(null));
+		Assert.assertFalse("Valida RG Nulo: ",rg.isValidaRG(""));
+		Assert.assertFalse("Valida RG sem traço: ",rg.isValidaRG("335073138"));
+		Assert.assertTrue("Valida RG com um traço: ", rg.isValidaRG("33507313-X"));
 	}
-
+*/
 }
