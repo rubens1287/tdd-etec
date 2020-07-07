b478e99b8794b39885cc16b7109c21ddb812c250
Rubens Lobo <rubens.lobo1287@yahoo.com.br>
2020-07-07 20:12:35 -0300

    teste faield 1

--- src/test/java/unittest/RgTest.java
+++ src/test/java/unittest/RgTest.java
@@ -9,8 +9,8 @@ public class RgTest {
 	RG rg = new RG();
 	@Test
 	public void testIsValidoRG() {
-		/*Assert.assertFalse("Valida RG Nulo: ",rg.isValidaRG(null));
-		Assert.assertFalse("Valida RG Vazio: ",rg.isValidaRG(""));
+		Assert.assertFalse("Valida RG Nulo: ",rg.isValidaRG(null));
+		/*Assert.assertFalse("Valida RG Vazio: ",rg.isValidaRG(""));
 		Assert.assertFalse("Valida RG sem traço: ",rg.isValidaRG("335073138"));
 		Assert.assertTrue("Valida RG com um traço: ", rg.isValidaRG("33507313-X"));*/
 	}
