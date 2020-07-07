cec139550acbbba0cb2ecef4bc8f6627fff7af33
Rubens Lobo <rubens.lobo1287@yahoo.com.br>
2020-07-07 20:08:38 -0300

    teste faield 1

--- src/main/java/tdd/RG.java
+++ src/main/java/tdd/RG.java
@@ -3,7 +3,7 @@ package tdd;
 public class RG {
 
     public boolean isValidaRG(String valor){
-        if((valor == null) ||  (valor.isEmpty()) ) {
+        if((valor == null) ||  (valor.isEmpty()) || (valor.charAt(8) != '-')) {
             return false;
         }
         return true;
